8dbc6ec90280775682cb95fe106f00170d8367fe
rtrauntvein <rtrauntvein@novacoast.com>
2014-04-24 10:17:31 -0700

    Php?

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+//Hacky
+?>
+<iframe frameborder="0" scrolling="no" 
+   src="../examples/platformer/index.html" name="gamebox" id="gamebox">
+   <p>iframes are not supported by your browser.</p>
+</iframe><br />
+
